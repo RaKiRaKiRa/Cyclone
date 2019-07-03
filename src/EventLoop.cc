5fbc8efd8f3c90568db89cdc46bfa9aa744c4a8e
RaKiRaKiRa <achen19951212@qq.com>
2019-07-03 23:40:53 +0800

    add support for timer(just declaration)

--- src/EventLoop.cc
+++ src/EventLoop.cc
@@ -2,7 +2,7 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-06-07 17:39
- * Last modified : 2019-06-15 21:53
+ * Last modified : 2019-07-03 20:34
  * Filename      : EventLoop.cc
  * Description   : 
  **********************************************************/
@@ -10,9 +10,22 @@
 #include "EventLoop.h"
 #include "IPoller.h"
 #include "Channel.h"
+#include "TimeQueue.h"
+
+#include <sys/eventfd.h>
 // 当前线程中的EventLoop, 保证one loop per thread
 __thread EventLoop *t_loopInThisThread = 0;
 
+int createEventfd()
+{
+  int fd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+  if(fd < 0)
+  {
+    LOG_ERROR << "Failed in eventfd";
+    abort();
+  }
+  return fd;
+}
 
 EventLoop::EventLoop(int timeout, poller type):
   PollerType(type),
@@ -21,7 +34,11 @@ EventLoop::EventLoop(int timeout, poller type):
   quit_(true),
   handling_(false),
   threadId_(CurrentThread::tid()),
-  poller_(IPoller::newPoller(this))
+  poller_(IPoller::newPoller(this)),
+  timeQueue_(new TimeQueue(this)),
+  wakeupFd_(createEventfd()),
+  wakeupChannel_(new Channel(this, wakeupFd_)),
+  calling_(false)
 {
   LOG <<"EventLoop created " << this <<" in thread " << threadId_;  
 
@@ -33,6 +50,8 @@ EventLoop::EventLoop(int timeout, poller type):
   {
     t_loopInThisThread = this;
   }
+  wakeupChannel_ -> setReadCallback(std::bind(&EventLoop::handleRead, this));
+  wakeupChannel_ -> enableRead();
 }
 
 EventLoop::~EventLoop()
@@ -72,9 +91,9 @@ void EventLoop::loop()
     activeChannels_.clear();
     poller_ -> poll(kPollTimeOut, &activeChannels_); 
     handling_ = true;
-    for(ChannelList::iterator it = activeChannels_.begin(); it != activeChannels_.end(); ++it)
+    for(Channel* it : activeChannels_)
     {
-      (*it) -> handleEvent();//it指向Channel*,   *it获得Channel*
+      it -> handleEvent();//it指向Channel*,   *it获得Channel*
     }
     handling_ = false;
 
@@ -89,7 +108,7 @@ void EventLoop::quit()
   quit_ = true;
 }
 
-
+//对Channel操作
 void EventLoop::updateChannel(Channel* channel)
 {
   assert(channel -> loop() == this);
@@ -107,4 +126,51 @@ void EventLoop::removeChannel(Channel* channel)
 bool EventLoop::hasChannel(Channel* channel) const
 {
   return poller_ -> hasChannel(channel);
+}
+
+
+//对定时器操作
+TimerId EventLoop::runAt(const int64_t& time, TimerCallback cb)
+{
+
+}
+
+TimerId EventLoop::runAfter(double delay, TimerCallback cb)
+{
+
+}
+
+TimerId EventLoop::runEvery(double interval, TimerCallback cb)
+{
+
+}
+
+void EventLoop::runInLoop(Functor cb)
+{
+
+}
+
+void EventLoop::queueInLoop(Functor cb)
+{
+
+}
+
+void EventLoop::cancel(TimerId timerId)
+{
+
+}
+
+void EventLoop::wakeup()
+{
+
+}
+
+void EventLoop::handleRead()
+{
+
+}
+
+void EventLoop::doPendingFunctors()
+{
+
 }
--- src/EventLoop.h
+++ src/EventLoop.h
@@ -2,7 +2,7 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-06-07 17:28
- * Last modified : 2019-06-19 15:46
+ * Last modified : 2019-07-03 20:34
  * Filename      : EventLoop.h
  * Description   : 
  **********************************************************/
@@ -16,9 +16,11 @@
 #include <pthread.h>
 #include <assert.h>
 #include <functional>
+#include "Timer.h"
 
 class Channel;
 class IPoller;
+class TimeQueue;
 
 enum poller
 {
@@ -31,6 +33,7 @@ class EventLoop : noncopyable
 {
 public:
   typedef std::function<void()> Functor;//用于定时器
+  typedef std::function<void()> TimerCallback;
   typedef std::vector<Channel*> ChannelList;//用于连接Channel
 
   //选择默认Poller
@@ -41,9 +44,24 @@ public:
   void quit();
 
   //Channel IPoller TimeQueue不直接相互调用,而是通过EventLoop中的控制函数进行间接交互
+  //
+  //对Channel操作
   void updateChannel(Channel* channel);
   void removeChannel(Channel* channel);
   bool hasChannel(Channel* channel) const;
+
+
+  //定时器相关
+  TimerId runAt(const int64_t& time, TimerCallback cb);
+  TimerId runAfter(double delay, TimerCallback cb);
+  TimerId runEvery(double interval, TimerCallback cb);
+  void runInLoop(Functor cb);
+  void queueInLoop(Functor cb);
+  void cancel(TimerId timerId);
+  void wakeup();//像wakeupFd_发送一个字节，使poll从阻塞中唤醒
+  void handleRead();//绑定给wakeupChannel_做可读监听，从wakeupFd_接收一个字节，使poll从阻塞状态唤醒
+  void doPendingFunctors(); 
+
 
   // 用于断言是否在Loop线程中进行
   bool isInLoopThread() const
@@ -52,11 +70,10 @@ public:
   }
 
   void assertInLoopThread();
-    
   static EventLoop* getEventLoopOfCurrentThread();
 
-  const poller PollerType;
 private:
+  const poller PollerType;
   const int kPollTimeOut;//EventLoop中的poll等待毫秒数
   void abortNotInLoopThread();
 
@@ -67,6 +84,12 @@ private:
 
   std::unique_ptr<IPoller> poller_;
   ChannelList activeChannels_;//由Poller::poll得到的
+
+  std::unique_ptr<TimeQueue> timeQueue_;
+  std::vector<Functor> pendingFunctors_;
+  int wakeupFd_;
+  std::unique_ptr<Channel> wakeupChannel_;
+  bool calling_;
 
 };
 
