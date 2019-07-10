6799092670feada67284fb5ac0bae804a981400d
RaKiRaKiRa <achen19951212@qq.com>
2019-07-10 23:22:00 +0800

    Package socket-fd with RAII methods

---
+++ src/Socket.cc
@@ -0,0 +1,48 @@
+/**********************************************************
+ * Author        : RaKiRaKiRa
+ * Email         : 763600693@qq.com
+ * Create time   : 2019-07-10 21:37
+ * Last modified : 2019-07-10 23:12
+ * Filename      : Socket.cc
+ * Description   : 
+ **********************************************************/
+
+#include "Socket.h"
+#include <memory.h>
+
+void Socket::bind(const struct sockaddr* addr)
+{
+  Bind(sockfd_, addr);
+}
+
+void Socket::listen()
+{
+  Listen(sockfd_);
+}
+
+int Socket::accept(sockaddr* addr)
+{
+  memset(addr, 0, sizeof(*addr));
+  int connfd = Accept(sockfd_, addr);
+  return connfd;
+}
+
+void Socket::shutdownWrite()
+{
+  ShutdownWrite(sockfd_);
+}
+
+void Socket::setNonblock(bool on)
+{
+  ::setNonblock(sockfd_, on);
+}
+
+void Socket::setReuseAddr(bool on)
+{
+  ::setReuseAddr(sockfd_, on);
+}
+
+void Socket::setReusePort(bool on)
+{
+  ::setReusePort(sockfd_, on);
+}
---
+++ src/Socket.h
@@ -0,0 +1,68 @@
+/**********************************************************
+ * Author        : RaKiRaKiRa
+ * Email         : 763600693@qq.com
+ * Create time   : 2019-07-09 22:26
+ * Last modified : 2019-07-10 21:37
+ * Filename      : Socket.h
+ * Description   : 
+ **********************************************************/
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include "base/noncopyable.h"
+#include "SocketOpts.h"
+
+// RAII 封装socket api
+class Socket : noncopyable
+{
+public:
+  Socket() :
+    sockfd_(createNonblockingSockfd())
+  {}
+
+  explicit Socket(int sockfd):
+    sockfd_(sockfd)
+  {}
+
+  ~Socket()
+  {
+    Close(sockfd_);
+  }
+
+  int fd() const
+  {
+    return sockfd_;
+  }
+
+/******************通信api************************/
+  void bind(const struct sockaddr* addr);
+
+  void listen();
+
+  int accept(sockaddr* addr);
+
+  //仅关闭写，可以接收完数据
+  void shutdownWrite();
+
+/******************设置api************************/
+  //非阻塞
+  void setNonblock(bool on);
+
+  //地址复用
+  void setReuseAddr(bool on);
+
+  //端口复用
+  void setReusePort(bool on);
+  
+  //Nagle
+  void setNodelay(bool on);
+
+  //TCP层心跳
+  void setKeepAlive(bool on);
+
+private:
+  const int sockfd_;
+};
+
+#endif
