55873821c6eb3a2fe4c49002d1b34315a3b3e75c
RaKiRaKiRa <achen19951212@qq.com>
2019-07-12 23:20:08 +0800

    Package Socket and Channel into RAII class for listening and creating new connections.

---
+++ src/Acceptor.cc
@@ -0,0 +1,79 @@
+/**********************************************************
+ * Author        : RaKiRaKiRa
+ * Email         : 763600693@qq.com
+ * Create time   : 2019-07-12 17:21
+ * Last modified : 2019-07-12 23:10
+ * Filename      : Acceptor.cc
+ * Description   : 
+ **********************************************************/
+
+#include "Acceptor.h"
+#include "Socket.h"
+
+Acceptor::Acceptor(EventLoop *loop, const sockaddr_in& addr, bool reuseport):
+  loop_(loop),
+  socket_(createNonblockingSockfd()),
+  channel_(loop, socket_.fd()),
+  listening_(false)
+{
+  //多路复用+回调绑定
+  socket_.setReuseAddr(true);
+  socket_.setReusePort(reuseport);
+  socket_.bind(&addr);
+  channel_.setReadCallback(std::bind(&Acceptor::handleRead, this));
+}
+
+// 将channel从loop和poll移除
+Acceptor::~Acceptor()
+{
+  channel_.disableAll();
+  channel_.remove();
+}
+
+//开始监听
+void Acceptor::listen()
+{
+  loop_ -> assertInLoopThread();
+  listening_ = true;
+  socket_.listen();
+  channel_.enableRead();
+}
+
+// 可读（accept）的回调,已有连接完成三次握手
+void Acceptor::handleRead()
+{
+  loop_ -> assertInLoopThread();
+  sockaddr_in addr;
+  //获得链接对应fd
+  //这里加上循环，accept到不能再accept，可以增加对短连接的接受率
+  while(1)
+  {
+    //获得一个链接fd
+    int connfd = socket_.accept(&addr);
+    if(connfd > 0)
+    {
+      if(newConnCallback_)
+      {
+        newConnCallback_(connfd, addr);
+      }
+      else
+      {
+        Close(connfd);
+      }
+    }
+    //no more
+    else if(connfd == -1 && errno == EAGAIN)
+    {
+      LOG_TRACE << "accepted all conn in Acceptor::handleRead";
+      break;
+    }
+    //connfd <= 0表示连接失败，出现错误 
+    else
+    {
+      LOG_ERROR << "in Acceptor::handleRead";
+      break;
+    }
+  } 
+
+}
+
---
+++ src/Acceptor.h
@@ -0,0 +1,49 @@
+/**********************************************************
+ * Author        : RaKiRaKiRa
+ * Email         : 763600693@qq.com
+ * Create time   : 2019-07-12 16:13
+ * Last modified : 2019-07-12 17:13
+ * Filename      : Acceptor.h
+ * Description   : 
+ **********************************************************/
+#ifndef ACCEPTOR_H
+#define ACCEPTOR_H
+
+#include "Socket.h"
+#include "EventLoop.h"
+#include "Channel.h"
+
+//将Socket与Channel再次封装成用于监听、接收新连接的RAII类
+class Acceptor : noncopyable
+{
+public:
+  typedef std::function<void(int sockfd, sockaddr_in& addr)> NewConnCallback;
+  Acceptor(EventLoop *loop, const sockaddr_in& addr, bool reuseport);
+
+  //析构将channel_从EventLoop和IPoller中移除
+  //Socket本身是RAII的，生命周期与Acceptor相同不用管
+  ~Acceptor();
+
+  void listen();
+  void setNewConnCallback(NewConnCallback cb)
+  {
+    newConnCallback_ = std::move(cb);
+  }
+
+  bool listening() const
+  {
+    return listening_;
+  }
+
+private:
+  // 给channel的ReadCallback进行绑定，对于异步socket在listen后通过(e)poll发生可读事件即为完成三次握手，可以进行accept
+  void handleRead();
+
+  EventLoop *loop_;
+  Socket socket_;
+  Channel channel_;
+  NewConnCallback newConnCallback_;//建立连接后的回调，用与建立TcpConnection
+  bool listening_;
+};
+
+#endif
