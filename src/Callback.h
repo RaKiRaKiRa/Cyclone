a07c56c21c040a64846f4a9be90c6f7daf29e754
RaKiRaKiRa <achen19951212@qq.com>
2019-07-17 00:05:46 +0800

    callback

--- src/Callback.h
+++ src/Callback.h
@@ -2,7 +2,7 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-06-30 17:33
- * Last modified : 2019-06-30 17:33
+ * Last modified : 2019-07-16 22:36
  * Filename      : Callback.h
  * Description   : 
  **********************************************************/
@@ -17,5 +17,13 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
 
+typedef std::function<void()> TimerCallback;
+
+
+class Buffer;
+class Connection;
+typedef std::shared_ptr<Connection> ConnectionPtr;
+
+typedef std::function<void (const ConnectionPtr&)> ConnCallback;//连接事件
 
 #endif
