38fdbdf7edc4446b8ee8f3eca18daf53a87ad2cc
Raki <763600693@qq.com>
2019-08-19 18:48:21 +0800

    test for HeartBeat

--- src/test/echo_client.cc
+++ src/test/echo_client.cc
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/tcp.h>
 
 int main()
 {
@@ -25,15 +26,22 @@ int main()
     printf("ok\n");
   }
   char buf[128];
-//  while(1)
-  //{
+  while(1)
+  {
     bzero(buf,sizeof(buf));
     scanf("%s",buf);
+    if(strcmp(buf,":quit") == 0)
+      break;
+    struct tcp_info info;
+    int len=sizeof(info);
+    getsockopt(sockfd, IPPROTO_TCP, TCP_INFO, &info, (socklen_t *)&len);
+    if((info.tcpi_state!=TCP_ESTABLISHED))
+      break;
     write(sockfd,buf,sizeof(buf));//写数据
     bzero(buf,sizeof(buf));
     read(sockfd,buf,sizeof(buf));//读数据
     printf("echo:%s\n",buf);
-  //}
+  }
   shutdown(sockfd,SHUT_WR);
  // close(sockfd);
   return 0;
---
+++ src/test/echo_heart.cc
@@ -0,0 +1,80 @@
+/**********************************************************
+ * Author        : RaKiRaKiRa
+ * Email         : 763600693@qq.com
+ * Create time   : 2019-08-19 15:44
+ * Last modified : 2019-08-19 15:44
+ * Filename      : echo_heart.cc
+ * Description   : 
+ **********************************************************/
+
+
+#include "../EventLoopThreadPool.h"
+#include "../base/Logging.h"
+#include "../serverWithHeartBeat.h"
+#include "../EventLoop.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+int numThreads = 0;
+
+
+AsyncLogging *logptr = NULL;
+void output(const char* logline, int len)
+{
+  logptr -> append(logline, len);
+}
+
+class EchoServer
+{
+public:
+  EchoServer(EventLoop* loop, const sockaddr_in& addr):
+    server_(loop, addr, "EchoServer")
+  {
+    server_.setConnCallback(std::bind(&EchoServer::onConnection, this, _1));
+    server_.setMessCallback(std::bind(&EchoServer::onMessage, this, _1, _2));
+    server_.setThreadNum(numThreads);
+  }
+
+  void start()
+  {
+    LOG_INFO << "starting " << numThreads <<" threads";
+    server_.start();
+  }
+
+  void onConnection(const ConnectionPtr& conn)
+  {
+    LOG_TRACE << toIpPort(conn -> peer()) << " -> " <<toIpPort(conn -> local())<<" is " <<(conn -> connected()?"UP":"DOWN");
+    conn -> setNoDelay(true);
+  }
+
+  // buf一般是Connection::inputBuffer_
+  void onMessage(const ConnectionPtr& conn, Buffer* buf)
+  {
+    conn -> send(buf);
+  }
+
+private:
+    serverWithHeartBeat server_;
+};
+
+
+
+int main(int argc, char* argv[])
+{
+  logptr = new AsyncLogging("asynclog", 50*1024*1000);
+  Logger::setOutput(output);
+  logptr -> start();
+
+  LOG_INFO <<"PID = " << getpid() <<", tid = " << CurrentThread::tid();
+  if(argc > 1)
+    numThreads = atoi(argv[1]);
+  EventLoop loop;
+  sockaddr_in addr;
+  fromPort(4567, &addr);
+  EchoServer server(&loop, addr);
+  server.start();
+
+  loop.loop();
+
+}
