c90135dd4a423a1cd178787ef6f2774f73434402
raki <RaKiRaKiRa>
2019-08-26 20:52:08 +0800

    output the LOG that is higer than minlevel_

--- src/base/Logging.h
+++ src/base/Logging.h
@@ -2,7 +2,7 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-06-02 20:16
- * Last modified : 2019-06-02 22:54
+ * Last modified : 2019-08-26 20:50
  * Filename      : Logging.h
  * Description   : 
  **********************************************************/
@@ -13,13 +13,21 @@
 #include "LogStream.h"
 #include "CurrentThread.h"
 #include <string>
-#define LOG Logger(__FILE__, __LINE__, __func__).stream()
-#define LOG_TRACE Logger(__FILE__, __LINE__, __func__, Logger::TRACE).stream()
-#define LOG_DEBUG Logger(__FILE__, __LINE__, __func__, Logger::DEBUG).stream()
-#define LOG_INFO  Logger(__FILE__, __LINE__, __func__, Logger::INFO ).stream()
-#define LOG_WARN  Logger(__FILE__, __LINE__, __func__, Logger::WARN ).stream()
-#define LOG_ERROR Logger(__FILE__, __LINE__, __func__, Logger::ERROR).stream()
-#define LOG_FATAL Logger(__FILE__, __LINE__, __func__, Logger::FATAL).stream()
+#define LOG if(Logger::logLevel() <= Logger::INFO) \
+    Logger(__FILE__, __LINE__, __func__).stream()
+#define LOG_TRACE  if(Logger::logLevel() <= Logger::TRACE) \
+    Logger(__FILE__, __LINE__, __func__, Logger::TRACE).stream()
+#define LOG_DEBUG  if(Logger::logLevel() <= Logger::DEBUG) \
+    Logger(__FILE__, __LINE__, __func__, Logger::DEBUG).stream()
+#define LOG_INFO   if(Logger::logLevel() <= Logger::INFO) \
+    Logger(__FILE__, __LINE__, __func__, Logger::INFO ).stream()
+#define LOG_WARN   if(Logger::logLevel() <= Logger::WARN) \
+    Logger(__FILE__, __LINE__, __func__, Logger::WARN ).stream()
+#define LOG_ERROR  if(Logger::logLevel() <= Logger::ERROR) \
+    Logger(__FILE__, __LINE__, __func__, Logger::ERROR).stream()
+#define LOG_FATAL  if(Logger::logLevel() <= Logger::FATAL) \
+    Logger(__FILE__, __LINE__, __func__, Logger::FATAL).stream()
+
 class AsyncLogging;
 
 class Logger
@@ -46,6 +54,14 @@ public:
   }
   static void setOutput(OutputFunc);
   static void setFlush(FlushFunc);
+  static void setLogLevel(Logger::LogLevel level)
+  {
+    minLevel_ = level;
+  }
+  static LogLevel logLevel() const
+  {
+    return minLevel_;
+  }
 private:
   class Impl
   {
@@ -59,8 +75,10 @@ private:
     LogLevel level_;
   };
   Impl impl_;
+  static LogLevel minLevel_;
   //static std::string logFileName;
 };
 
+Logger::LogLevel Logger::minLevel_ = Logger::TRACE;
 
 #endif
