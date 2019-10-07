c3df5ba96f9de58a23364feffc39dd81293d72a7
raki <RaKiRaKiRa>
2019-10-07 20:34:27 +0800

    Connector Complete

--- src/Connector.cc
+++ src/Connector.cc
@@ -2,20 +2,213 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-10-07 00:44
- * Last modified : 2019-10-07 00:54
+ * Last modified : 2019-10-07 20:32
  * Filename      : Connector.cc
  * Description   : 
  **********************************************************/
 
 #include "Connector.h"
-#include "EventLoop.h"
 #include "Channel.h"
+#include "EventLoop.h"
+#include "base/Logging.h"
+#include "errno.h"
 
-Connector::Connector(Eventloop* loop, sockaddr_in& addr):
+Connector::Connector(EventLoop* loop, sockaddr_in& addr):
   loop_(loop),
   serverAddr_(addr),
   started_(false),
-  state_(kDisconnected)
+  state_(kDisconnected),
+  retryDelayMs_(kInitRetryDelayMs)
 {
   LOG_DEBUG << "connector [" << this <<"]";
+}
+
+Connector::~Connector()
+{
+  if(!id_.isNull)
+  {
+    loop_->cancel(id_);
+  }
+    
+  LOG_DEBUG << "~connector [" << this <<"]";
+}
+
+void Connector::start()
+{
+  assert(!started_);
+  started_ = true;
+  loop_->runInLoop(std::bind(&Connector::startInLoop, this));
+}
+
+// 会被start()和retry()调用，
+// connect失败都调用retry()，retry会过一会儿再重新startInLoop
+// 这段时间内可能会stop，故判断是否开启连接
+void Connector::startInLoop()
+{
+  assert(state_ == kDisconnected);
+  if(!id_.isNull())
+    id_.setNull();
+  // 判断是否启动，用于retry放入定时器后，调用重连时判断
+  if(started_)
+  {
+    connect();
+  }
+  else
+  {
+    LOG_DEBUG << "is not connect";
+  }
+}
+
+void Connector::connect()
+{
+  int sockfd = createNonblockingSockfd();
+  int ret = Connect(sockfd, &serverAddr_);
+  int savedErrno = (ret == 0) ? 0 : errno;
+  switch (savedErrno)
+  {
+    // 等待连接建立的情况
+    case 0:
+    case EINPROGRESS:     //套接字为非阻塞套接字，且连接请求没有立即完成
+    case EINTR:           //系统调用的执行由于捕获中断而中止
+    case EISCONN:         //已经连接到该套接字
+      connecting(sockfd);
+      break;
+
+    //稍后重试的情况
+    case EAGAIN:          //没有足够空闲的本地端口
+    case EADDRINUSE:      //本地地址处于使用状态
+    case EADDRNOTAVAIL:   //
+    case ECONNREFUSED:    //远程地址并没有处于监听状态
+    case ENETUNREACH:     //网络不可到达
+      retry(sockfd);
+      break;
+
+    case EACCES:           //用户试图在套接字广播标志没有设置的情况下连接广播地址或由于防火墙策略导致连接失败。
+    case EPERM:            //用户试图在套接字广播标志没有设置的情况下连接广播地址或由于防火墙策略导致连接失败。
+    case EAFNOSUPPORT:     //参数serv_add中的地址非合法地址
+    case EALREADY:         //套接字为非阻塞套接字，并且原来的连接请求还未完成
+    case EBADF:            //非法的文件描述符
+    case EFAULT:           //指向套接字结构体的地址非法
+    case ENOTSOCK:         //文件描述符不与套接字相关
+      LOG_ERROR << "connect error in Connector::startInLoop " << savedErrno;
+      Close(sockfd);
+      break;
+
+    default:
+      LOG_ERROR << "Unexpected error in Connector::startInLoop " << savedErrno;
+      Close(sockfd);
+      break;
+  }
+}
+
+// 根据建立的连接生成Channel 判断连接是否可写,再handleWrite中继续下一步
+void Connector::connecting(int sockfd)
+{
+  setState(kConnecting);
+  assert(!channel_);
+  channel_.reset(new Channel(loop_, sockfd));
+  channel_->setWriteCallback(std::bind(&Connector::handleWrite, this));
+  channel_->setErrorCallback(std::bind(&Connector::handleError, this));
+  channel_->enableWrite();
+}
+
+// 可读表示连接已建立
+void Connector::handleWrite()
+{
+  LOG_TRACE << "Connector::handleWrite " << state_;
+  if(state_ == kConnecting)
+  {
+    int sockfd = removeAndResetChannel();
+    int err = getSocketError(sockfd);
+    //无错返回0
+    if(err)
+    {
+      LOG_WARN << "Connector::handleWrite - ERROR = "<< err << " " << strerror(err);
+      retry(sockfd);
+    }
+    else
+    {
+      setState(kConnected);
+      if(started_)
+      {
+        newConnCallback_(sockfd);
+      }
+      else
+      {
+        Close(sockfd);
+      }
+    }
+  }
+  else
+  {
+    LOG_DEBUG << "Connector::handleWrite is not kConnecting, it is " << state_;
+  }
+}
+
+
+void Connector::stop()
+{
+  started_ = false;
+  loop_->runInLoop(std::bind(&Connector::stopInLoop, this));
+}
+
+void Connector::stopInLoop()
+{
+  // 将正在连接改为断开连接
+  // 若已连接，则无视
+  // 断开连接操作由Client和Connection进行
+  if(state_ == kConnecting)
+  {
+    setState(kConnected);
+    int sockfd = removeAndResetChannel();
+    // 防止断开期间又开启
+    retry(sockfd);
+  }
+}
+
+void Connector::retry(int sockfd)
+{
+  Close(sockfd);
+  setState(kDisconnected);
+  if(started_)
+  {
+    LOG_INFO << "Connector::retry - Retry connecting to " << toIpPort(serverAddr_) << "in " << retryDelayMs_ << " ms";
+    id_ = loop_->runAfter(retryDelayMs_ / 1000.0, std::bind(&Connector::startInLoop, this));
+    retryDelayMs_ *= 2;
+    if(retryDelayMs_ > kMaxRetryDelayMs)
+      retryDelayMs_ = kMaxRetryDelayMs;  
+  }
+  else
+  {
+    LOG_DEBUG << "do not connect";
+  }
+}
+
+int Connector::removeAndResetChannel()
+{
+  channel_->disableAll();
+  channel_->remove();
+  int sockfd = channel_->fd();
+  // 本函数会在handleWrite里调用，也就是说我们正处于Channel::handleEvent
+  // 故不能在这个reset
+  loop_->queueInLoop(std::bind(&Connector::resetChannel, this));
+  return sockfd;
+}
+
+void Connector::resetChannel()
+{
+  channel_.reset();
+}
+
+// 报错并retry
+void Connector::handleError()
+{
+  LOG_ERROR << "Connector::handleError state = " << state_;
+  if(state_ == kConnecting)
+  {
+    int sockfd = removeAndResetChannel();
+    int err = getSocketError(sockfd);
+    LOG_TRACE << "SO_ERROR = " << strerror(err);
+    retry(sockfd);
+  }
 }
--- src/Connector.h
+++ src/Connector.h
@@ -2,7 +2,7 @@
  * Author        : RaKiRaKiRa
  * Email         : 763600693@qq.com
  * Create time   : 2019-10-06 23:15
- * Last modified : 2019-10-07 00:54
+ * Last modified : 2019-10-07 20:32
  * Filename      : Connector.h
  * Description   : 
  **********************************************************/
@@ -12,18 +12,18 @@
 #include "Socket.h"
 #include "base/noncopyable.h"
 #include "SocketOpts.h"
-#include "base/Logging.h"
 #include <functional>
 #include <memory>
 
-class Eventloop;
+class EventLoop;
 class Channel;
 // 对应Acceptor，主动发起连接，发起的连接用Connection封装，供Client使用
+// socket是一次性的，一旦出错无法恢复。仅仅能关闭重来。使用新的fd后，用新的channel。
 class Connector: noncopyable, std::enable_shared_from_this<Connector>
 {
 public:
   typedef std::function<void(int)> NewConnCallback;
-  Connector(Eventloop* loop, sockaddr_in& addr);
+  Connector(EventLoop* loop, sockaddr_in& addr);
 
   ~Connector();
 
@@ -32,8 +32,14 @@ public:
     newConnCallback_ = std::move(cb);
   }
 
-  void start();
-  void stop();
+  //Connector::start()-->Connector::startInLoop()-->Connector::connect()-->Connector::connecting()
+  //然后通过判断是否可写 调用handleWrite判断连接是否成功
+  //若成功，则先removeAndResetChannel,然后newConnCallback_让Connection接管连接
+  void start();  
+  // 将正在连接改为断开连接
+  // 若已连接，则无视
+  // 断开连接操作由Client和Connection进行 
+  void stop();      
 
 private:
   enum State
@@ -55,20 +61,27 @@ private:
 
   void startInLoop();
   void stopInLoop();
+  // 建立连接
   void connect();
-  void connectComplete(int sockfd);
+  // 根据建立的连接生成Channel 判断连接是否可写,在handleWrite中继续下一步
+  void connecting(int sockfd);
   void handleWrite();
   void handleError();
-  void removeAndResetChannel();// 不能直接reset，需加入runInLoop,在下一次loop中调用，因为正处于Channel::handleEvent
+  int  removeAndResetChannel();// 不能直接reset，需加入runInLoop,在下一次loop中调用，因为正处于Channel::handleEvent
   void resetChannel();
+  void retry(int sockfd);
 
-
-  Eventloop* loop_;
+  EventLoop* loop_;
   sockaddr_in& serverAddr_;
   bool started_;
   State state_;
   std::unique_ptr<Channel> channel_;
   NewConnCallback newConnCallback_; // 用于建立连接后创造Connection
+  TimerId id_;
+  int retryDelayMs_;
+
+  static const int kMaxRetryDelayMs = 30*1000;
+  static const int kInitRetryDelayMs = 500;
 };
 
 #endif
